1e9faf49b279b109512d353c0fa517e633263402
Szymon Skorupa <skorupas29@gmail.com>
2022-09-15 16:37:49 +0200

    cleanup

--- mp3_file_rename.py
+++ mp3_file_rename.py
@@ -5,17 +5,12 @@ dir_path = r'F:\\ProgramData\\ABLETON_MEDIA\\Ableton Renders\\spotify_recorder\\
 
 res = []
 
-# Iterate directory
 for path in os.listdir(dir_path):
-    # check if current path is a file
     if os.path.isfile(os.path.join(dir_path, path)):
         res.append(path)
-# print(res)
 
 for file_name in res:
-    print(file_name)
     audiofile = eyed3.load(f"{dir_path}\\{file_name}")
-    print(file_name.split(" - ").pop().split(".mp3",1)[0])
     audiofile.tag.artist = file_name.split(" - ").pop().split(".mp3",1)[0]
     title = file_name.split(" - ")[:-1]
     title = ' - '.join(title)
